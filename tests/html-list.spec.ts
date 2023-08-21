4d617b785757d2abef03a512693b6720948f7d60
Mykola Grybyk <mykola.grybyk@gmail.com>
2023-08-21 22:24:53 +0200

    test failure

--- tests/html-list.spec.ts
+++ tests/html-list.spec.ts
@@ -8,7 +8,7 @@ test('html list', async ({ page }) => {
     await expect(page.locator('#links')).toBeVisible()
     const links = ['..', 'main', 'feature-branch']
     for (const link of links) {
-        await expect(page.locator(`#links a[href="${link}"]`)).toHaveText(link)
+        await expect(page.locator(`#links a[href="${link}"]`)).toHaveText(link + '_bug')
     }
 
     await expect(page).toHaveScreenshot()

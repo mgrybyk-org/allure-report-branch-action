3dabee1118a61daea6cf415a575020d12925d929
Mykola Grybyk <25589559+mgrybyk@users.noreply.github.com>
2024-02-01 19:15:33 +0100

    cleanup (#14)

---
+++ src/cleanup.ts
@@ -0,0 +1,64 @@
+import * as path from 'path'
+import * as fs from 'fs/promises'
+import { spawnProcess } from './spawnProcess.js'
+import { normalizeBranchName } from './helpers.js'
+import { Context } from '@actions/github/lib/context.js'
+
+export const cleanupOutdatedBranches = async (ghPagesBaseDir: string, repo: Context['repo']) => {
+    try {
+        const prefix = 'refs/heads/'
+        // for some reason git won't pick up config, using url for now
+        const lsRemote = await spawnProcess(
+            'git',
+            ['ls-remote', '--heads', `https://github.com/${repo.owner}/${repo.repo}.git`],
+            process.env.GITHUB_WORKSPACE
+        )
+        const remoteBranches = lsRemote
+            .split('\n')
+            .filter((l) => l.includes(prefix))
+            .map((l) => normalizeBranchName(l.split(prefix)[1]))
+
+        const localBranches = (await fs.readdir(ghPagesBaseDir, { withFileTypes: true })).filter((d) => d.isDirectory()).map((d) => d.name)
+
+        for (const localBranch of localBranches) {
+            if (!remoteBranches.includes(localBranch)) {
+                await fs.rm(path.join(ghPagesBaseDir, localBranch), { recursive: true, force: true })
+            }
+        }
+    } catch (err) {
+        console.error('cleanup outdated branches failed.', err)
+    }
+}
+
+export const cleanupOutdatedReports = async (ghPagesBaseDir: string, maxReports: number) => {
+    try {
+        const localBranches = (await fs.readdir(ghPagesBaseDir, { withFileTypes: true })).filter((d) => d.isDirectory()).map((d) => d.name)
+
+        // branches
+        for (const localBranch of localBranches) {
+            const reports = (await fs.readdir(path.join(ghPagesBaseDir, localBranch), { withFileTypes: true }))
+                .filter((d) => d.isDirectory())
+                .map((d) => d.name)
+
+            // report per branch
+            for (const reportName of reports) {
+                const runs = (await fs.readdir(path.join(ghPagesBaseDir, localBranch, reportName), { withFileTypes: true }))
+                    .filter((d) => d.isDirectory())
+                    .map((d) => d.name)
+
+                // run per report
+                if (runs.length > maxReports) {
+                    runs.sort()
+                    while (runs.length > maxReports) {
+                        await fs.rm(path.join(ghPagesBaseDir, localBranch, reportName, runs.shift() as string), {
+                            recursive: true,
+                            force: true,
+                        })
+                    }
+                }
+            }
+        }
+    } catch (err) {
+        console.error('cleanup outdated reports failed.', err)
+    }
+}
--- src/helpers.ts
+++ src/helpers.ts
@@ -1,7 +1,9 @@
 import type { context } from '@actions/github'
 
+export const normalizeBranchName = (branchName: string) => branchName.replaceAll('/', '_').replaceAll('.', '_')
+
 export const getBranchName = (gitRef: string, pull_request?: (typeof context)['payload']) => {
     const branchName: string = pull_request ? pull_request.head.ref : gitRef.replace('refs/heads/', '')
 
-    return branchName.replaceAll('/', '_').replaceAll('.', '_')
+    return normalizeBranchName(branchName)
 }
